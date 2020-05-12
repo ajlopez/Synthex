0b63b86fa41e173c98dc3068e41f583ab730c139
ajlopez <webmaster@ajlopez.com>
2020-05-12 17:46:44 -0300

    Synth tests refactor

--- test/Synth_tests.js
+++ test/Synth_tests.js
@@ -5,14 +5,16 @@ contract('Synth', function (accounts) {
     const alice = accounts[0];
     const bob = accounts[1];
     const charlie = accounts[2];
+    
+    beforeEach(async function () {
+        this.synth = await Synth.new('SYM1', 'Name', Buffer.from('TOK1'));
+    });
 
     it('create synth', async function () {
-        const synth = await Synth.new('SYM1', 'Name', Buffer.from('TOK1'));
-        
-        const name = await synth.name();
-        const decimals = await synth.decimals();
-        const symbol = await synth.symbol();
-        const key = await synth.key();
+        const name = await this.synth.name();
+        const decimals = await this.synth.decimals();
+        const symbol = await this.synth.symbol();
+        const key = await this.synth.key();
         
         assert.equal(name, 'Name');
         assert.equal(decimals, 18);
@@ -21,11 +23,9 @@ contract('Synth', function (accounts) {
     });
 
     it('issue synth', async function () {
-        const synth = await Synth.new('SYM1', 'Name', Buffer.from('TOK1'));
+        await this.synth.issue(bob, 1000);
         
-        await synth.issue(bob, 1000);
-        
-        const result = await synth.balanceOf(bob);
+        const result = await this.synth.balanceOf(bob);
         
         assert.equal(result, 1000);
     });
