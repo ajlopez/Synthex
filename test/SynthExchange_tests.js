59857a7de4aae6905c7d9c055cee84d20af516d3
ajlopez <webmaster@ajlopez.com>
2020-05-13 14:46:52 -0300

    SynthExchange tests refactor

--- test/SynthExchange_tests.js
+++ test/SynthExchange_tests.js
@@ -8,29 +8,31 @@ contract('SynthExchange', function (accounts) {
     const bob = accounts[1];
     const charlie = accounts[2];
     
-    beforeEach(async function () {
-        this.synthexchange = await SynthExchange.new();
-    });
+    describe('SynthExchange', function () {
+        beforeEach(async function () {
+            this.synthexchange = await SynthExchange.new();
+        });
 
-    it('initial synthex', async function () {
-        const synthex = await this.synthexchange.synthex();
-        
-        assert.equal(synthex, 0);
-    });
+        it('initial synthex', async function () {
+            const synthex = await this.synthexchange.synthex();
+            
+            assert.equal(synthex, 0);
+        });
 
-    it('set synthex', async function () {
-        await this.synthexchange.setSynthex(bob);
-        
-        const synthex = await this.synthexchange.synthex();
-        
-        assert.equal(synthex, bob);
-    });
+        it('set synthex', async function () {
+            await this.synthexchange.setSynthex(bob);
+            
+            const synthex = await this.synthexchange.synthex();
+            
+            assert.equal(synthex, bob);
+        });
 
-    it('only owner can set synthex', async function () {
-        expectThrow(this.synthexchange.setSynthex(bob, { from: charlie }));
-        
-        const synthex = await this.synthexchange.synthex();
-        
-        assert.equal(synthex, 0);
+        it('only owner can set synthex', async function () {
+            expectThrow(this.synthexchange.setSynthex(bob, { from: charlie }));
+            
+            const synthex = await this.synthexchange.synthex();
+            
+            assert.equal(synthex, 0);
+        });
     });
 });
