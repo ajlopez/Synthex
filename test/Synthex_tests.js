a40403f3303aa9b2614f2f7477634ac585d24cda
ajlopez <webmaster@ajlopez.com>
2020-05-13 13:09:16 -0300

    Synthex tests refactor

--- test/Synthex_tests.js
+++ test/Synthex_tests.js
@@ -8,46 +8,46 @@ contract('Synthex', function (accounts) {
     const alice = accounts[0];
     const bob = accounts[1];
     const charlie = accounts[2];
+
+    beforeEach(async function () {
+        this.synthex = await Synthex.new();
+    });
     
     it('unknown synth', async function () {
-        const synthex = await Synthex.new();
         const synth = await Synth.new('SYM1', 'Name', Buffer.from('TOK1'));
 
-        const result = await synthex.synths(await synth.key());
+        const result = await this.synthex.synths(await synth.key());
         
         assert.equal(result, 0);
     });
 
     it('add synth', async function () {
-        const synthex = await Synthex.new();
         const synth = await Synth.new('SYM1', 'Name', Buffer.from('TOK1'));
 
-        await synthex.addSynth(synth.address);
+        await this.synthex.addSynth(synth.address);
         
-        const result = await synthex.synths(await synth.key());
+        const result = await this.synthex.synths(await synth.key());
         
         assert.equal(result, synth.address);
     });
     
     it('cannot add synth twice', async function () {
-        const synthex = await Synthex.new();
         const synth = await Synth.new('SYM1', 'Name', Buffer.from('TOK1'));
 
-        await synthex.addSynth(synth.address);
-        expectThrow(synthex.addSynth(synth.address));
+        await this.synthex.addSynth(synth.address);
+        expectThrow(this.synthex.addSynth(synth.address));
         
-        const result = await synthex.synths(await synth.key());
+        const result = await this.synthex.synths(await synth.key());
         
         assert.equal(result, synth.address);
     });
     
     it('only owner can add synth', async function () {
-        const synthex = await Synthex.new();
         const synth = await Synth.new('SYM1', 'Name', Buffer.from('TOK1'));
 
-        expectThrow(synthex.addSynth(synth.address, { from: bob }));
+        expectThrow(this.synthex.addSynth(synth.address, { from: bob }));
         
-        const result = await synthex.synths(await synth.key());
+        const result = await this.synthex.synths(await synth.key());
         
         assert.equal(result, 0);
     });
